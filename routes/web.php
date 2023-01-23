66efec47dd7da01b03cecdf0c5bce5f5c4e0a5e6
Floz8 <55977382+Floz8@users.noreply.github.com>
2023-01-23 09:32:29 +0100

    commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\EvenementController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -18,14 +21,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+Route::get('dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('welcome', [EvenementController::class, 'list']);
 
 require __DIR__.'/auth.php';
